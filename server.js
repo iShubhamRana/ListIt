14d1912c9d88df8934207f33d3a5d0cfe61deced
Shubham <rana2001shubham@gmail.com>
2021-04-15 18:13:07 +0530

    'procfile'

--- server.js
+++ server.js
@@ -239,6 +239,10 @@ app.post('/logout' , (req , res)=>{
   res.redirect('/');
 })
 
-app.listen(3000 , ()=>{
-  console.log('server has started at port 3000');
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port , ()=>{
+  console.log('server has started');
+});
